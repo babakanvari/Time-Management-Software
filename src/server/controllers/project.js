fd929283cc49157ce45ea79de56ff6990cd8bf44
babakanvari <babak@Babaks-MacBook-Pro.local>
2020-05-18 14:34:57 -0700

    refactor(project): apply renaming

--- src/server/controllers/project.js
+++ src/server/controllers/project.js
@@ -1,12 +1,12 @@
-let projectRouter = require('express').Router();
+let router = require('express').Router();
 import { Project } from '../entities/project';
 import * as projectServices from '../services/project';
 import ash from '../middleware/asyncHandler';
 
-projectRouter.post('/', ash(async (req, res, next) => {
+router.post('/', ash(async (req, res, next) => {
     let project = new Project(req.body);
     project = await projectServices.create(project, next);
     res.send(project);
 }));
 
-module.exports = projectRouter;
+module.exports = router;
