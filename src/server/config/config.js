79039c025fa847d3bb614d5dd5f637495feb7183
babakanvari <babak@Babaks-MacBook-Pro.local>
2020-06-04 22:34:54 -0700

    feat: create environment config files
    closes #3

---
+++ src/server/config/config.js
@@ -0,0 +1,22 @@
+var config = {
+  dev: 'development',
+  test: 'testing',
+  prod: 'production',
+  port: process.env.PORT || 7777,
+  secret: process.env.SECRET || 'BABAK'
+};
+
+process.env.NODE_ENV = process.env.NODE_ENV || config.dev;
+config.env = process.env.NODE_ENV;
+
+var envConfig;
+
+try {
+  envConfig = require('./' + config.env);
+  envConfig = envConfig || {};
+
+} catch (e) {
+  envConfig = {};
+}
+
+module.exports = Object.assign(config, envConfig);
